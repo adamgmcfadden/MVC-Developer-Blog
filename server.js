a5b3ea86ac9cb1046bfe1bbf2cbe6d40f1f3f671
Adam Girard-McFadden <adam_m_20@hotmail.com>
2021-08-20 13:21:34 -0400

    completed importing/config - created main.handlebars for generic html

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // --------------DEPENDENCIES------------------- // IMPORT AS REQUIRED TO SIMPLIFY CODE
 // npm packages
 const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 // import from other files
 const sequelize = require("./config/connection");
@@ -10,9 +13,29 @@ const app = express();
 // PORT variable for listener
 const PORT = process.env.PORT || 3001;
 
-//middleware
+//config session
+const sess = {
+  secret: "Chamber of Secrets",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+//express. use session
+app.use(session(sess));
+
+// Sets Handlebars as the default template engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//other middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(require("./controllers/"));
 
 //listener - sync sequelize to express
 sequelize.sync({ force: false }).then(() => {
